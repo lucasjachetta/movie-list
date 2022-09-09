aea4389dd930cbaecd52ee42d66ce64e38d3153e
lucasjachetta <lucas.jachetta@gmail.com>
2022-09-09 18:12:53 -0300

    Rotas

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+
+function App() {
+ 
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Login/>}/>
+            <Route path='/home' element={<Home/>}/>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './pages/login/Login';
+import App from './App';
 import GlobalStyle from './globalStyle';
-
+import { Routes } from 'react-router-dom';
 
 
 
@@ -10,8 +10,9 @@ import GlobalStyle from './globalStyle';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Routes/>
     <GlobalStyle />
-    <Login />
+    <App />
   </React.StrictMode>
 );
 
